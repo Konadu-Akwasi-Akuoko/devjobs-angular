01a29b59943c54c2424524a5cfecb775be2d5409
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-19 15:08:58 +0000

    feat: setup tailwind theme, to support dark and light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { ITheme } from './store/theme/theme.reducer';
+import { setTheme } from './store/theme/theme.action';
 
 @Component({
   selector: 'app-root',
@@ -14,17 +15,19 @@ import { ITheme } from './store/theme/theme.reducer';
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'devjobs-angular';
+  theme: 'light' | 'dark' = 'light';
 
   theme$: Observable<ITheme>;
   themeSubscription!: Subscription;
 
   constructor(private store: Store<{ theme: ITheme }>) {
     this.theme$ = store.select('theme');
+    store.dispatch(setTheme({ theme: 'light' }));
   }
 
   ngOnInit(): void {
     this.themeSubscription = this.theme$.subscribe((theme) => {
-      console.log(theme.theme);
+      this.theme = theme.theme;
     });
   }
 
