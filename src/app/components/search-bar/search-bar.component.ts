b64e6ddd0b911c42a14b909c948d29e76a6a3383
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-24 13:54:29 +0000

    feat: extracted the search-bar component, into it's own standalone componet

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormInputComponent } from '../form-input/form-input.component';
+import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { FilterButtonComponent } from '../filter-button/filter-button.component';
+import { debounceTime, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    FormInputComponent,
+    CheckboxComponent,
+    FilterButtonComponent,
+  ],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css',
+})
+export class SearchBarComponent implements OnInit, OnDestroy {
+  companyFormControl = new FormControl('');
+  companyName = 'company';
+  companyIconSrc = '/assets/desktop/icon-search.svg';
+
+  locationFormControl = new FormControl('');
+  locationName = 'location';
+  locationIconSrc = '/assets/desktop/icon-location.svg';
+
+  checkBoxFormControl = new FormControl(false);
+
+  searchFormGroup = new FormGroup({
+    company: this.companyFormControl,
+    location: this.locationFormControl,
+    isFullTime: this.checkBoxFormControl,
+  });
+
+  formGroupSubscription!: Subscription;
+
+  ngOnInit(): void {
+    this.formGroupSubscription = this.searchFormGroup.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        console.log(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.formGroupSubscription.unsubscribe();
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,51 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, Subscription } from 'rxjs';
-import { CheckboxComponent } from '../../components/checkbox/checkbox.component';
-import { FilterButtonComponent } from '../../components/filter-button/filter-button.component';
-import { FormInputComponent } from '../../components/form-input/form-input.component';
+import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    FormInputComponent,
-    CheckboxComponent,
-    FilterButtonComponent,
-  ],
+  imports: [SearchBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  companyFormControl = new FormControl('');
-  companyName = 'company';
-  companyIconSrc = '/assets/desktop/icon-search.svg';
-
-  locationFormControl = new FormControl('');
-  locationName = 'location';
-  locationIconSrc = '/assets/desktop/icon-location.svg';
-
-  checkBoxFormControl = new FormControl(false);
-
-  searchFormGroup = new FormGroup({
-    company: this.companyFormControl,
-    location: this.locationFormControl,
-    isFullTime: this.checkBoxFormControl,
-  });
-
-  formGroupSubscription!: Subscription;
-
-
-  ngOnInit(): void {
-    this.formGroupSubscription = this.searchFormGroup.valueChanges
-      .pipe(debounceTime(500))
-      .subscribe((value) => {
-        console.log(value);
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.formGroupSubscription.unsubscribe();
-  }
-}
+export class HomeComponent {}
