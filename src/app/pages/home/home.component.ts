13192defbb7122c0d31b03c3a058ae1783591789
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-24 13:49:06 +0000

    feat: added form controls to the form group

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, Subscription } from 'rxjs';
 import { CheckboxComponent } from '../../components/checkbox/checkbox.component';
 import { FilterButtonComponent } from '../../components/filter-button/filter-button.component';
 import { FormInputComponent } from '../../components/form-input/form-input.component';
@@ -16,7 +17,7 @@ import { FormInputComponent } from '../../components/form-input/form-input.compo
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   companyFormControl = new FormControl('');
   companyName = 'company';
   companyIconSrc = '/assets/desktop/icon-search.svg';
@@ -26,4 +27,25 @@ export class HomeComponent {
   locationIconSrc = '/assets/desktop/icon-location.svg';
 
   checkBoxFormControl = new FormControl(false);
+
+  searchFormGroup = new FormGroup({
+    company: this.companyFormControl,
+    location: this.locationFormControl,
+    isFullTime: this.checkBoxFormControl,
+  });
+
+  formGroupSubscription!: Subscription;
+
+
+  ngOnInit(): void {
+    this.formGroupSubscription = this.searchFormGroup.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        console.log(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.formGroupSubscription.unsubscribe();
+  }
 }
