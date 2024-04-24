d36ccfb05ed2451011bc5b8ea0b9c6a1170bb31a
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-24 01:52:58 +0000

    feat: created a setInitialState action for the job reducer

---
+++ src/app/store/jobs/jobs.action.ts
@@ -0,0 +1,3 @@
+import { createAction } from '@ngrx/store';
+
+export const setInitialJobsData = createAction('[Jobs] Set Initial Jobs Data');
--- src/app/store/jobs/jobs.reducer.ts
+++ src/app/store/jobs/jobs.reducer.ts
@@ -1,5 +1,17 @@
+import { createReducer } from '@ngrx/store';
 import { allJobDataType } from '../../../lib/types/types';
+import { immerOn } from 'ngrx-immer/store';
+import { setInitialJobsData } from './jobs.action';
 
 export interface IJobsData {
   jobs: allJobDataType[];
 }
+
+export const initialState: IJobsData = {
+  jobs: [],
+};
+
+export const jobsReducer = createReducer(
+  initialState,
+  immerOn(setInitialJobsData, (state: IJobsData) => {})
+);
