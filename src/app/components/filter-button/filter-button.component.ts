3bad256cef9239f654b072c497d9689bf3fb399c
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-24 09:39:06 +0000

    added an input field for the mobile search bar

---
+++ src/app/components/filter-button/filter-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-button.component.html',
+  styleUrl: './filter-button.component.css'
+})
+export class FilterButtonComponent {
+
+}
