cbf3ee4a3124556da2bdba10d3f0a4d62ef81690
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-25 08:25:10 +0000

    feat: created the description section in the job-id page

--- src/app/components/job-id/description/description.component.ts
+++ src/app/components/job-id/description/description.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-description',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './description.component.css'
 })
 export class DescriptionComponent {
+
+  @Input ({ required: true }) description: string = '';
 
 }
