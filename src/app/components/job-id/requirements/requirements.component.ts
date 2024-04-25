9587a6b6518ae130bb52e92ef3f262981b034552
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-25 08:31:21 +0000

    feat: created the requirements component for the job-id page

--- src/app/components/job-id/requirements/requirements.component.ts
+++ src/app/components/job-id/requirements/requirements.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-requirements',
   standalone: true,
   imports: [],
   templateUrl: './requirements.component.html',
-  styleUrl: './requirements.component.css'
+  styleUrl: './requirements.component.css',
 })
 export class RequirementsComponent {
-
+  @Input({ required: true }) requirements: {
+    content: string;
+    items: string[];
+  } = { content: '', items: [] };
 }
