41c8307cb447fb6efe1ea6a0653fb83e4c6b26b6
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-25 08:37:29 +0000

    feat: created the what-will-you-do component

--- src/app/components/job-id/what-will-you-do/what-will-you-do.component.ts
+++ src/app/components/job-id/what-will-you-do/what-will-you-do.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-what-will-you-do',
   standalone: true,
   imports: [],
   templateUrl: './what-will-you-do.component.html',
-  styleUrl: './what-will-you-do.component.css'
+  styleUrl: './what-will-you-do.component.css',
 })
 export class WhatWillYouDoComponent {
-
+  @Input({ required: true }) roles: {
+    content: string;
+    items: string[];
+  } = { content: '', items: [] };
 }
