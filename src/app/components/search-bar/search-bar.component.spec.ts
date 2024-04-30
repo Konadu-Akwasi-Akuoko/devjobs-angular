aa7177c07b5eda97bc0b07b60ad5827808eb997e
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-30 14:04:42 +0000

    test: added a test to the search-bar component

--- src/app/components/search-bar/search-bar.component.spec.ts
+++ src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { SearchBarComponent } from './search-bar.component';
-import { provideMockStore } from '@ngrx/store/testing';
+import { setFilteredJobs } from '../../store/jobs/jobs.action';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  let store: MockStore;
 
   const initialState = {
     isFullTimeInput: false,
@@ -21,16 +22,57 @@ describe('SearchBarComponent', () => {
 
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    component.searchInputs = {
-      isFullTimeInput: false,
-      locationInput: '',
-      titleCompanyExpertiseInput: '',
+    expect(component).toBeTruthy();
+  });
+
+  it('should update searchInputs when store emits new value', () => {
+    const mockInputs = {
+      isFullTimeInput: true,
+      locationInput: 'Test Location',
+      titleCompanyExpertiseInput: 'Test Input',
     };
-    
-    // expect(component).toBeTruthy();
+    store.setState({ searchInputs: mockInputs });
+    store.refreshState();
+    expect(component.searchInputs).toEqual(mockInputs);
+  });
+
+  it('should dispatch setFilteredJobs action when form value changes', () => {
+    const dispatchSpy = jest.spyOn(store, 'dispatch');
+    component.searchFormGroup.setValue({
+      company: 'Test Company',
+      location: 'Test Location',
+      isFullTime: true,
+    });
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+
+  it('should dispatch setFilteredJobs action when onSearchButtonClick is called', () => {
+    const dispatchSpy = jest.spyOn(store, 'dispatch');
+    component.onSearchButtonClick();
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+
+  it('should dispatch setFilteredJobs action with default values when form control values are null', () => {
+    const dispatchSpy = jest.spyOn(store, 'dispatch');
+
+    // Set form control values to null
+    component.companyFormControl.setValue(null);
+    component.locationFormControl.setValue(null);
+    component.checkBoxFormControl.setValue(null);
+
+    component.onSearchButtonClick();
+
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      setFilteredJobs({
+        companyTitleExpertise: '',
+        location: '',
+        isFullTime: false,
+      })
+    );
   });
 });
