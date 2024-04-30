06105546a3e205a3a352e44a9becfa7afc66733b
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-04-30 14:31:32 +0000

    test: added a test to home component

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { provideStore } from '@ngrx/store';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [provideStore({})]
     })
     .compileComponents();
     
--- src/app/store/search-inputs/search-inputs.service.spec.ts
+++ src/app/store/search-inputs/search-inputs.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideMockActions } from '@ngrx/effects/testing';
 import { SearchInputsService } from './search-inputs.service';
+import { Observable } from 'rxjs';
+import { Action } from '@ngrx/store';
+
+const actions$ = new Observable<Action>();
 
 describe('SearchInputsService', () => {
   let service: SearchInputsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideMockActions(() => actions$)],
+    });
     service = TestBed.inject(SearchInputsService);
   });
 
